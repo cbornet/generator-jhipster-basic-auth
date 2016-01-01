8487d51ea6a7af60079cd19f1f2e09323b1a5df9
cbornet <cbornet@hotmail.com>
2016-01-01 21:54:29 +0000

    initial import

---
+++ README.md
@@ -0,0 +1,39 @@
+# generator-jhipster-basic-auth
+[![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> JHipster module, Adds stateless basic authentication support
+
+# Introduction
+
+This is a [JHipster](http://jhipster.github.io/) module, that is meant to be used in a JHipster application.
+
+# Prerequisites
+
+As this is a [JHipster](http://jhipster.github.io/) module, we expect you have JHipster and its related tools already installed:
+
+- [Installing JHipster](https://jhipster.github.io/installation.html)
+
+# Installation
+
+To install this module:
+
+```bash
+npm install -g generator-jhipster-basic-auth
+```
+
+To update this module:
+```bash
+npm update -g generator-jhipster-basic-auth
+```
+
+# Usage
+
+# License
+
+Apache-2.0 © [Christophe Bornet]
+
+[npm-image]: https://img.shields.io/npm/v/generator-jhipster-basic-auth.svg
+[npm-url]: https://npmjs.org/package/generator-jhipster-basic-auth
+[travis-image]: https://travis-ci.org/cbornet/generator-jhipster-basic-auth.svg?branch=master
+[travis-url]: https://travis-ci.org/cbornet/generator-jhipster-basic-auth
+[daviddm-image]: https://david-dm.org/cbornet/generator-jhipster-basic-auth.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/cbornet/generator-jhipster-module
---
+++ generators/app/index.js
@@ -0,0 +1,68 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var packagejs = require(__dirname + '/../../package.json');
+
+// Stores JHipster variables
+var jhipsterVar = {moduleName: 'basic-auth'};
+
+// Stores JHipster functions
+var jhipsterFunc = {};
+
+module.exports = yeoman.generators.Base.extend({
+
+  initializing: {
+    templates: function (args) {
+      this.composeWith('jhipster:modules', {
+        options: {
+          jhipsterVar: jhipsterVar,
+          jhipsterFunc: jhipsterFunc
+        }
+      });
+    },
+    displayLogo: function () {
+      // Have Yeoman greet the user.
+      this.log(yosay(
+        'Welcome to the ' + chalk.red('JHipster basic-auth') + ' generator! ' + chalk.yellow('v' + packagejs.version)
+      ));
+    }
+  },
+
+  prompting: function () {
+    var done = this.async();
+
+    var prompts = [{
+      type: 'input',
+      name: 'message',
+      message: 'Your project files will be modified. Are you sure you want to continue ? (y/N)',
+      default: 'N'
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      // To access props later use this.props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  writing: function () {
+    var done = this.async();
+
+    this.baseName = jhipsterVar.baseName;
+    this.packageName = jhipsterVar.packageName;
+    this.angularAppName = jhipsterVar.angularAppName;
+    var javaDir = jhipsterVar.javaDir;
+    var resourceDir = jhipsterVar.resourceDir;
+    var webappDir = jhipsterVar.webappDir;
+
+    if(this.props.message.toUpperCase() !== 'Y') {
+        process.exit(1);
+    }
+
+    this.template('src/main/java/package/config/_BasicAuthSecurityConfiguration.java', javaDir + '/config/BasicAuthSecurityConfiguration.java', this, {});
+
+    done();
+  }
+});
